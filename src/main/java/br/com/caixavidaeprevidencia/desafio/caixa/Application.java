33831b475cf5420df7be17cea0d067e6921713d8
Jonatas Coelho <jonatascoelho1993@gmail.com>
2025-02-24 10:57:23 -0300

    Configuração swagger

--- src/main/java/br/com/caixavidaeprevidencia/desafio/caixa/Application.java
+++ src/main/java/br/com/caixavidaeprevidencia/desafio/caixa/Application.java
@@ -1,4 +1,4 @@
-package br.com.caixavidaeprevidencia.desafio_caixa;
+package br.com.caixavidaeprevidencia.desafio.caixa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
