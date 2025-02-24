19709ec35f028f7928707fc5879b63c510dede32
Jonatas Coelho <jonatascoelho1993@gmail.com>
2025-02-24 10:43:51 -0300

    inicial commit

---
+++ src/test/java/br/com/caixavidaeprevidencia/desafio_caixa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caixavidaeprevidencia.desafio_caixa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
